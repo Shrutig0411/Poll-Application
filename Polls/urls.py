948b4fab3658f81974dadb12c7930e0a108ad0b9
Shrutig0411 <shrutiguptashruti01@gmail.com>
2021-06-06 20:04:41 +0530

    Changed url pattern

--- Polls/urls.py
+++ Polls/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 app_name = 'Polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:ques_id>/', views.detail, name='detail'),
-    path('<int:ques_id>/results/', views.results, name='results'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(),name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:ques_id>/vote/', views.vote, name='vote'),
 ]
