32abe9edbef88a12c5b2533d3382c5ccc6fcd3a9
Shrutig0411 <shrutiguptashruti01@gmail.com>
2021-06-06 16:40:07 +0530

    urls added for newly written views

--- Polls/urls.py
+++ Polls/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
-
+app_name = 'Polls'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:ques_id>/', views.detail, name='detail'),
+    path('<int:ques_id>/results/', views.results, name='results'),
+    path('<int:ques_id>/vote/', views.vote, name='vote'),
 ]
