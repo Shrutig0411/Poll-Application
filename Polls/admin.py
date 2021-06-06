71eac99538d752a12c2b9a2fca6c19ffbc4e6870
Shrutig0411 <shrutiguptashruti01@gmail.com>
2021-06-07 01:40:00 +0530

    Change to admin site and added choice model

--- Polls/admin.py
+++ Polls/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
-admin.site.register(Question)
+from .models import Question, Choice
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['ques_txt']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('ques_txt', 'pub_date','was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['ques_txt']
+
+admin.site.register(Question, QuestionAdmin)
