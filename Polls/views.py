24bad64bba3a7f6470ce8b91646de655bced35a1
Shrutig0411 <shrutiguptashruti01@gmail.com>
2021-06-06 16:39:06 +0530

    Few more views added

--- Polls/views.py
+++ Polls/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Question
+from django.template import loader
+from django.http import HttpResponse
+from django.http import Http404
 
 # Create your views here.
-from django.http import HttpResponse
-
 def index(request):
-    return HttpResponse("Hey! We are at polls index")
+    recently_added_ques=Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('Polls/index.html')
+    context = {
+        'recently_added_ques': recently_added_ques,
+    }
+    return render(request, 'Polls/index.html', context)
+
+
+def detail(request,ques_id):
+    question=get_object_or_404(Question, pk=ques_id)
+    return render(request, 'Polls/details.html', {'question': question}) 
+
+def results(request,ques_id):
+    return HttpResponse("You are looking at results for question %s." % ques_id) 
+
+def vote(request,ques_id):
+    return HttpResponse("You are voting on question %s." % ques_id)     
